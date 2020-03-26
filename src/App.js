ab98824d0036b0286ff02ecd89034e2cfe0f5406
SKHolmes <samualkholmes@gmail.com>
2020-03-26 22:08:55 +1300

    Committing local game

--- src/App.js
+++ src/App.js
@@ -1,30 +1,81 @@
 import React, { useState, useEffect } from 'react';
-import { GoogleLogin } from 'react-google-login';
+import Button from '@material-ui/core/Button';
+import './style.css';
 
 function App() {
-  const [signedIn, setSignedIn] = useState([]);
+  const [cards, setCards] = useState([]);
+  const [consoleText, setConsoleText] = useState('');
+  const [ruleText, setRuleText] = useState('');
+  const [players, setPlayers] = useState(['Henry', 'Sam', 'Daniel']);
 
-  const googleLoginSuccess = response => {
-    console.log(response.profileObj.name);
-    setSignedIn([...signedIn, response.profileObj.name]);
-  };
-
-  const googleLoginFailure = response => {
-    console.log('Something went wrong with the login. Ask the big man.');
+  const rules = {
+    Ace: 'In Your face',
+    King: 'Vessel',
+    Queen: 'Question Master',
+    Jack: 'Waterfall',
+    Ten: 'Boat Came Into The Harbor',
+    Nine: 'Bust a Rhyme',
+    Eight: 'Mates',
+    Seven: 'Rule Card',
+    Six: 'Dicks',
+    Five: 'Voldemort',
+    Four: 'Quarantine',
+    Three: 'Nominate Three',
+    Two: 'Snake Eyes?'
   };
 
   useEffect(() => {
-    console.log(`signedIn has changed, it is now ${signedIn}`);
-  }, [signedIn]);
+    generateDeck();
+    //generatePlayers();
+    // eslint-disable-next-line
+  }, []);
+
+  const generateDeck = function() {
+    let deck = [];
+    setConsoleText('New Game');
+    setRuleText('New Game');
+    for (const rule in rules) {
+      console.log(rule);
+      deck.push({ card: `${rule} of Hearts`, rule: rules[rule] });
+      deck.push({ card: `${rule} of Diamonds`, rule: rules[rule] });
+      deck.push({ card: `${rule} of Clubs`, rule: rules[rule] });
+      deck.push({ card: `${rule} of Spades`, rule: rules[rule] });
+    }
+    setCards(shuffle(deck));
+  };
+
+  const draw = function() {
+    const drawn = cards.shift();
+    const player = players.shift();
+    players.push(player);
+    setConsoleText(`${player} drew the ${drawn.card}`);
+    setRuleText(drawn.rule);
+  };
+
+  const shuffle = function(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+    }
+    return a;
+  };
 
   return (
-    <GoogleLogin
-      clientId='121431919294-gd4pja6n0j259ntk3jhe70h49i24cf78.apps.googleusercontent.com'
-      buttonText='Login'
-      onSuccess={googleLoginSuccess}
-      onFailure={googleLoginFailure}
-      cookiePolicy={'single_host_origin'}
-    />
+    <>
+      <div className='center'>
+        <p>Cards left {cards.length}</p>
+        {cards.length ? (
+          <Button onClick={draw}>Draw</Button>
+        ) : (
+          <Button onClick={generateDeck}>New Game</Button>
+        )}
+        <div>{consoleText}</div>
+        <h3>{ruleText}</h3>
+      </div>
+    </>
   );
 }
 
